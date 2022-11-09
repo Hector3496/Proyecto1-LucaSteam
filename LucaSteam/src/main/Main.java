b2682cc1177172b48acae72b853b493913442fe3
Hector3496 <48091519+Hector3496@users.noreply.github.com>
2022-11-09 18:06:18 +0100

    Actualizar clase main

--- LucaSteam/src/main/Main.java
+++ LucaSteam/src/main/Main.java
@@ -1,10 +1,11 @@
 package main;
 
+import gui.AbrirTienda;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AbrirTienda.abrir();
 	}
 
 }
