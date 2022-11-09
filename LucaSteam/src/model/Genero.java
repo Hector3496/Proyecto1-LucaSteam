a6c49cc33972f15e37c1ecb3a5a12aa52ea93084
sdiador <117722284+sdiador@users.noreply.github.com>
2022-11-09 14:06:27 +0100

    Add files via upload

---
+++ LucaSteam/src/model/Genero.java
@@ -0,0 +1,18 @@
+package model;
+
+public enum Genero {
+	
+	Sports,
+	Platform,
+	Role_Playing,
+	Puzzle,
+	Misc,
+	Shooter,
+	Simulation,
+	Racing,
+	Action,
+	Adventure,
+	Fighting,
+	Strategy;
+
+}
